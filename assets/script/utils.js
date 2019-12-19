c223ac22ace02bf94bf4ed1c9a49cd3d029d559f
liusisi <liusisi@163.com>
2019-12-19 21:46:23 +0800

    push current files

---
+++ assets/script/utils.js
@@ -0,0 +1,29 @@
+let C = {};
+
+//获取鼠标在元素上的坐标
+C.getOffset = function(ele){
+  let mouse = {x:0, y:0};
+  ele.addEventListener('mousemove', function(e) {
+    let { x, y } = C.eventWrapper(e);
+    mouse.x = x;
+    mouse.y = y;
+  })
+  return mouse;
+}
+
+//坐标系转换
+C.eventWrapper = function(ev) {
+  let { pageX, pageY, target } = ev;
+  let { left, top} = target.getBoundingClientRect();
+  return { x : pageX-left, y: pageY-top};
+}
+
+//角度转弧度
+C.toRad = function(angle){
+  return angle * Math.PI / 180
+}
+
+//弧度转角度
+C.toAngle = function(rad){
+  return rad * 180 / Math.PI
+}
