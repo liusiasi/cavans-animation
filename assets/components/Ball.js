d03220dc0103cc36b801e81e2dd23dd97da14307
liusisi <liusisi@163.com>
2020-01-02 10:26:26 +0800

    gravity fedding

---
+++ assets/components/Ball.js
@@ -0,0 +1,29 @@
+class Ball {
+  constructor(props){
+    this.x = 0;
+    this.y = 0;
+    this.r = 20;
+    this.scaleX = 1;
+    this.scaleY = 1;
+    this.strokeStyle = 'rgba(0, 0, 0, 0)';
+    this.fillStyle = 'rgb(57, 119, 224)';
+    this.alpha = 1;
+    Object.assign(this, props);
+    return this;
+  }
+  render(ctx){
+    let {x, y, r, scaleX, scaleY, fillStyle, strokeStyle, alpha} = this;
+    ctx.save();
+    ctx.translate(x,y);
+    ctx.scale(scaleX,scaleY);
+    ctx.strokeStyle = strokeStyle;
+    ctx.fillStyle = fillStyle;
+    ctx.globalAlpha = alpha;
+    ctx.beginPath();
+    ctx.arc(0,0,r,0,2*Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();
+    return this;
+  }
+}
